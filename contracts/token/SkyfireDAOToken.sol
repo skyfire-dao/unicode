8b5738c512cd4abf79ef5377deddf95c1a33eec8
crypticnfk <89737792+crypticnfk@users.noreply.github.com>
2021-11-06 14:25:37 +0530

    Created skyfire dao token

--- contracts/token/SkyfireDAOToken.sol
+++ contracts/token/SkyfireDAOToken.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.7.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestERC20 is ERC20, Ownable {
-    constructor (string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-        _mint(msg.sender, 10000000000000000000 * 10 ** decimals());
+contract SkyfireDAOToken is ERC20, Ownable {
+    constructor() ERC20("Skyfire DAO Token", "SDT") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
